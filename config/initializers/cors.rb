a68259759dc79495e946fcb36d537e29ee4ebd69
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-04-02 16:54:53 +0300

    sorting cors issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,17 @@
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins "https://realtor-frontend-puce.vercel.app"
+
+#     resource "*",
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head],
+#       credentials: true
+#   end
+# end
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://realtor-frontend-puce.vercel.app"
-
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
